a29f1276c7e152d123298126c0897895e9415ef6
Seeps <supersonic08@gmail.com>
2021-03-06 14:38:43 -0500

    Update findaspx.ps1

--- findaspx.ps1
+++ findaspx.ps1
@@ -1,9 +1,9 @@
-#@Seeps
+#@github.com/Seeps
 
-Write-Host "`nThis script will search for recently created .aspx files owned by the SYSTEM user`nPlease run it with Administrator rights`nAccess denied errors may occur due to the lack of permissions for the user context the script is running under (typically occurs for hidden files/folders)"
+Write-Host "`nThis script will search for recently created .aspx files owned by the SYSTEM user`nPlease run it with Administrator rights`nAccess denied errors may occur due to the lack of permissions for the user context the script is running under (typically occurs for hidden files/folders)`n`nNOTE: This has not been thoroughly tested; use at your own risk!`n"
 
 $SYSDRIVE = Read-Host -Prompt 'Enter the drive letter (default is C)'
 
-$FindDate=Get-Date -Year 2021 -Month 02 -Day 24
+$DATE=Get-Date -Year 2021 -Month 02 -Day 24
 
-Get-Childitem -Path ${SYSDRIVE}:\ -Include *.aspx -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { $_.LastWriteTime -ge $FindDate } | Get-Acl | Select-Object Owner,Path | Where-Object Owner -like *system
+Get-Childitem -Path ${SYSDRIVE}:\ -Include *.aspx -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { $_.LastWriteTime -ge $DATE } | Get-Acl | Select-Object Owner,Path | Where-Object Owner -like *system
