8ec688734d43451e378a5f1deaf60c83b23253c9
Seeps <supersonic08@gmail.com>
2021-03-09 17:21:24 -0500

    Update findaspx.ps1

--- findaspx.ps1
+++ findaspx.ps1
@@ -8,5 +8,5 @@ $DATE=Get-Date -Year 2021 -Month 01 -Day 01
 
 New-Item ${SYSDRIVE}:\CIR -type directory
 
-Get-Childitem -Path ${SYSDRIVE}:\ -Include *.aspx, *.asmx, *.php -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { $_.LastWriteTime -ge $DATE } | Get-Acl | Select-Object Owner,Path | Where-Object Owner -like *system | GCI | select FullName >> ${SYSDRIVE}:\CIR\filepaths.txt 
+Get-Childitem -Path ${SYSDRIVE}:\ -Include *.aspx, *.asmx, *.php -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { $_.LastWriteTime -ge $DATE } | Get-Acl | Where-Object Owner -like *system | Select-Object Path -ExpandProperty Access | select Path | Format-Table -AutoSize -Wrap >> ${SYSDRIVE}:\CIR\filepath.txt 
 Get-Childitem -Path ${SYSDRIVE}:\ -Include *.aspx, *.asmx, *.php -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { $_.LastWriteTime -ge $DATE } | Get-Acl | Select-Object Owner,Path | Where-Object Owner -like *system | Copy-Item -Destination ${SYSDRIVE}:\CIR
